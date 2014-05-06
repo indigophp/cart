fa7f83523fe9b868638e837b72588bb45e61a1b7
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-05-06 15:14:38 +0200

    Cart tests improved

--- tests/Test/CartTest.php
+++ tests/Test/CartTest.php
@@ -10,7 +10,7 @@ use Indigo\Cart\Store\SessionStore;
  * Tests for Cart
  *
  * @author  Márk Sági-Kazár <mark.sagikazar@gmail.com>
- * @coversDefaultClass  Indigo\Cart\Cart
+ * @coversDefaultClass  \Indigo\Cart\Cart
  */
 class CartTest extends \PHPUnit_Framework_TestCase
 {
@@ -18,32 +18,55 @@ class CartTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $data = array(
-            'b89a9e2ee493c37d2e090e54f7f79f3a' => new Item(array(
-                'id'       => 1,
-                'name'     => 'Some Product',
-                'price'    => 1.000,
-                'quantity' => 1,
-                'tax'      => 27,
-            ))
-        );
+        $this->cart = new Cart('cart_01');
 
         $store = new SessionStore;
-        $store->save('cart_01', $data);
 
-        $this->cart = new Cart('cart_01');
+        $store->load($this->cart);
+
+        $this->cart->add(
+            new Item(
+                array(
+                    'id'       => 1,
+                    'name'     => 'Some Product',
+                    'price'    => 1.000,
+                    'quantity' => 1,
+                    'tax'      => 27,
+                )
+            )
+        );
     }
 
+    /**
+     * @covers ::__construct
+     * @covers ::getId
+     * @group  Cart
+     */
+    public function testInstance()
+    {
+        $cart = new Cart('test');
+
+        $this->assertEquals('test', $cart->getId());
+
+        $cart = new Cart();
+
+        $this->assertStringStartsWith('__CART__', $cart->getId());
+    }
+
+    /**
+     * @covers ::add
+     * @group  Cart
+     */
     public function testNewAdd()
     {
-        $this->cart->setTax(27);
-
-        $item = new Item(array(
-            'id'       => 2,
-            'name'     => 'Some Other Product',
-            'price'    => 2.000,
-            'quantity' => 1,
-        ));
+        $item = new Item(
+            array(
+                'id'       => 2,
+                'name'     => 'Some Other Product',
+                'price'    => 2.000,
+                'quantity' => 1,
+            )
+        );
 
         $id = $item->getId();
 
@@ -56,15 +79,21 @@ class CartTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($item, $this->cart->get($id));
     }
 
+    /**
+     * @covers ::add
+     * @group  Cart
+     */
     public function testAdd()
     {
-        $item = new Item(array(
-            'id'       => 1,
-            'name'     => 'Some Product',
-            'price'    => 1.000,
-            'quantity' => 1,
-            'tax'      => 27,
-        ));
+        $item = new Item(
+            array(
+                'id'       => 1,
+                'name'     => 'Some Product',
+                'price'    => 1.000,
+                'quantity' => 1,
+                'tax'      => 27,
+            )
+        );
 
         $id = $item->getId();
 
@@ -77,24 +106,29 @@ class CartTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(2, $currentItem->quantity);
     }
 
+    /**
+     * @covers ::getTotal
+     * @group  Cart
+     */
     public function testTotal()
     {
-        $this->assertEquals(1, $this->cart->getTotal(false));
-        $this->assertEquals(1.27, $this->cart->getTotal());
+        $this->assertEquals(1, $this->cart->getTotal());
+        $this->assertEquals(1.27, $this->cart->getTotal(true));
     }
 
+    /**
+     * @covers ::getTax
+     * @group  Cart
+     */
     public function testTax()
     {
-        $this->assertEquals(0.27, $this->cart->getTotalTax());
-
-        $cart = $this->cart->setTax(27);
-        $this->assertInstanceOf('Indigo\\Cart\\Cart', $cart);
-        $this->assertTrue(is_int($this->cart->getTax()));
-
-        $this->cart->setTax(1, false);
-        $this->assertTrue(is_float($this->cart->getTax()));
+        $this->assertEquals(0.27, $this->cart->getTax());
     }
 
+    /**
+     * @covers ::getQuantity
+     * @group  Cart
+     */
     public function testQuantity()
     {
         $this->assertEquals(1, $this->cart->getQuantity());
