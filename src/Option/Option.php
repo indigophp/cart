4daf6923f06f40f43e47a5795b96128af42f1651
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-05-10 06:54:20 +0200

    Adds Option

---
+++ src/Option/Option.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Indigo Cart package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Cart\Option;
+
+use Indigo\Container\Struct;
+use Fuel\Common\Arr;
+use Serializable;
+
+/**
+ * Cart item option class
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class Option extends Struct implements OptionInterface, Serializable
+{
+    use \Indigo\Container\Helper\Serializable;
+
+    /**
+     * {@inheritdocs}
+     */
+    protected $struct = array(
+        'id' => array('type' => array('integer', 'string')),
+        'name' => array(
+            'required',
+            'type' => 'string',
+        ),
+        'value' => array('type' => 'float'),
+        'order' => array('type' => 'int'),
+    );
+
+    /**
+     * Keys to ignore in the hashing process
+     *
+     * @var array
+     */
+    protected $ignoreKeys = array('order');
+
+    /**
+     * {@inheritdocs}
+     */
+    public function getId()
+    {
+        // Filter ignored keys
+        $hashData = Arr::filterKeys($this->data, $this->ignoreKeys, true);
+
+        return md5(serialize($hashData));
+    }
+
+    /**
+     * {@inheritdocs}
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
